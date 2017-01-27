9a3cd5087b9f801153d24fe6e665700f26feca89
Unknown <migu27studios@gmail.com>
2017-01-27 10:56:30 -0700

    Added More and Rotate scripts to objects

---
+++ New Unity Project 2/Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(-.5f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(.5f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.Translate(0, -0.5f, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(0, 0.5f, 0);
+        }
+    }
+}
