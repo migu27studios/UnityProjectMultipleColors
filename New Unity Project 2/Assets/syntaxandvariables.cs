58acd8094fb795f5d32cd83f0c1595598ff4b7b0
Unknown <migu27studios@gmail.com>
2017-02-01 18:56:26 -0700

    Added in-class syntax 2

---
+++ New Unity Project 2/Assets/syntaxandvariables.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class syntaxandvariables : MonoBehaviour {
+
+    public string myString = "10";
+    public string myotherstring = "1";
+    public int myint = 1;
+    public float myfloat = 1.2f;
+    public bool mybool = false;
+
+
+	// Use this for initialization
+	void Start () {
+        print("Hello World");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
