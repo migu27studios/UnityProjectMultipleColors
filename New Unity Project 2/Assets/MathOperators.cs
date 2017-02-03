32c2f7a0f7e0faaeef0142f3c58307b7258f5e5c
Unknown <migu27studios@gmail.com>
2017-02-03 10:41:43 -0700

    Made script for math operators
    Addition, Subtraction, multiplication, and remainder

---
+++ New Unity Project 2/Assets/MathOperators.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathOperators : MonoBehaviour {
+
+    public int addition = 12 + 4;
+    public int subtraction = 12 - 4;
+    public float division = 5.0f / 2.0f;
+    public int multiplication = 10 * 2;
+    public int remainder = 7 % 2;
+
+
+	// Use this for initialization
+	void Start () {
+        addition += 9;       //+= adds an additional value to the int.
+        subtraction -= 10; //Subtract from.
+        addition += 3;
+        addition = 1;
+        
+
+	}
+	
+	
+}
