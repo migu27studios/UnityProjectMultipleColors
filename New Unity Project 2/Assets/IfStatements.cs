45f4570bb6f1bc16451ce1294a13075f72d50aa2
Unknown <migu27studios@gmail.com>
2017-02-06 10:48:54 -0700

    Added If statementd lecture

---
+++ New Unity Project 2/Assets/IfStatements.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour {
+
+    public bool myBool;
+    public string myString = "Dog";
+    public int num1;
+    public int num2;
+
+
+	// Use this for initialization
+	void Start () {
+        if (myBool)
+        {
+            print(true);
+        }
+
+        if (myString == "Cat")
+        {
+            print("Cat");
+
+        }
+        if (!myBool)
+        {
+            print("false");
+        }
+        if(myString != "Cat")
+        {
+            print(myString);
+        }
+        
+        if(num1 + num2 == 7)
+        {
+            print(7);
+        }
+        if(num1 + num2 != 7)
+        {
+            print(num1 + num2);
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
